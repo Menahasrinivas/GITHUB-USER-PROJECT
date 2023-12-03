6479b894b5760f4ae46b4eb1024ff317c50a94f3
Menahasrinivas <menahasrinivas@gmail.com>
2023-12-03 14:38:53 +0530

    commit changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const userInput = document.getElementById("userName");
 const getDeatailsButton = document.getElementById("getDetails");
 const profileInfo = document.getElementById("profileInfo");
-const repoinfo = document.getElementById("repoInfo");
+const repoInfo = document.getElementById("repoInfo");
 getDeatailsButton.addEventListener("click",async() =>{
     const userName = userInput.value;
     //console/log(userName);
@@ -15,17 +15,16 @@ getRepo(userName);
 //!Get profile function it is used to get the used details from the serveer.
 function getProfile(data){
     console.log(data);
-    profileInfo.innerHtml =`<div class ="card">
+    profileInfo.innerHTML =`<div class ="card">
     <div class ="card-img">
-    <img src=${data.avatar_url} alt=${data.name}>
+    <img src=${data.avatar_url} class="card-img-top img" alt=${data.name}>
     </div>
     <div class = "card-body">
-    <div class = "card-title">${data.name}</div>
-    <div class = "card-subHeading">${data.login}</div>
-    <div class = "card-text">
-    <p>${data.bio}</p>
-    <p><i class="fa-solid fa-users" style="color: #461f51;"></i>${data.followers}Followers ${data.following}Following</p>
-<p><i class="fa-solid fa-magnifying-glass-location" style="color: #411e76;"></i>${data.location}</p>
+    <h5 class = "card-title">${data.name || "No Name"}<h5>
+    <h5 class = "card-subHeading">${data.login || "No Login"}<h5>
+    <h5 class = "card-text">${data.bio || "No Bio"}<h5>
+    <p><i class="fa-solid fa-user-group"></i>${data.followers} Followers ${data.following}Following</p>
+<p><i class="fa-solid fa-location-dot"></i>${data.location}</p>
 <button>
 <a href = ${data.html_url} target="_blank">Visit Profile</a>
 </button>
@@ -39,13 +38,15 @@ async function getRepo(userName){
 const res = await fetch(`https://api.github.com/users/${userName}/repos`);
 const projects = await res.json();
 for(let i=0;i<projects.length;i++){
-    repoinfo.innerHtml +=`<div class = "card">
+    repoInfo.innerHTML +=`
+    <div class ="col-sm-10 col-md-6 col-12 col-lg-4">
+    <div class = "card">
     <div class ="card-body">
     <div class = "card-title">${projects[i].name}</div>
     <div class ="card-subHeading">${projects[i].language}</div>
-    <div class="card-text">
+    <div class="d-flex justify-content-center">
     <button>
-    <a href = ${projects[i].html_url} target="_blank">Visit Repo</a>
+    <a href = "${projects[i].html_url}" target="_blank" class="btn btn-primary">Visit Repo</a>
     </button>
     </div>
     </div>
